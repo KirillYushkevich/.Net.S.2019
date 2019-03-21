e5e0394d308633a87d6743fb6f90a404a74d3c35
KirillYushkevich <47444317+KirillYushkevich@users.noreply.github.com>
2019-03-21 19:28:38 +0300

    Added multi-file

---
+++ NET.S.2019.Yushkevich.01/Multi-file Builds/AnswerChecked.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaBeneAnketa.data
+{
+    public class AnswerChecked
+    {
+        public string Text { get; set; }
+        public bool Checked { get; set; }
+        public AnswerChecked (string text,bool cheked)
+        {
+            Text = text;
+            Checked = cheked;
+        }
+    }
+}
---
+++ NET.S.2019.Yushkevich.01/Multi-file Builds/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaBeneAnketa.data
+{
+   
+    public class Question
+    {
+        public string Problem { get; set; }
+        public bool Answerd { get; set; }
+        public string Answer { get; set; }
+        public Question(string text)
+        {
+            Problem = text;
+        }
+    }
+}
